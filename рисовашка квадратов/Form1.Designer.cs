aaabb6e8c4213a3d9b12187e530a7c9a87eaaeaa
Gladi1 <49594144+Gladi1@users.noreply.github.com>
2019-04-26 18:31:49 +0400

    1
    1

---
+++ рисовашка квадратов/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace рисовашка_квадратов
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(895, 587);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(917, 434);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 74);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "рисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1005, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1005, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(117, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1005, 178);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(117, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(950, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Высота";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(950, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ширина";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(950, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Стороны";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1166, 611);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Рисовашка квадратов";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ рисовашка квадратов/Form1.cs
@@ -0,0 +1,76 @@
+﻿//Автор Дамир Алмаев Маратович
+//библиотеки
+using System;//библиотека
+using System.Collections.Generic;//библиотека
+using System.ComponentModel;//библиотека
+using System.Data;//библиотека
+using System.Drawing;//библиотека
+using System.Linq;//библиотека
+using System.Text;//библиотека
+using System.Threading.Tasks;//библиотека
+using System.Windows.Forms;//библиотека
+//Автор Дамир Алмаев Маратович
+namespace рисовашка_квадратов//Как называется прога
+{
+    public partial class Form1 : Form// В какой форме работаем
+    {
+        //Автор Дамир Алмаев Маратович
+        //переменные
+        //Автор Дамир Алмаев Маратович
+        public static int x, y, a = 0;//для коридинатов
+        //Автор Дамир Алмаев Маратович
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)//для рисования
+        {
+            e.Graphics.DrawLine(System.Drawing.Pens.Red, x, y, x, y + a);//рисуем
+            e.Graphics.DrawLine(System.Drawing.Pens.Red, x, y, x + a, y);//рисуем
+            e.Graphics.DrawLine(System.Drawing.Pens.Red, x + a, y, x + a, y + a);//рисуем
+            e.Graphics.DrawLine(System.Drawing.Pens.Red, x, y + a, x + a, y + a);//рисуем
+        }
+        //Автор Дамир Алмаев Маратович
+        private void Form1_Load(object sender, EventArgs e)//При загруски формы
+        {
+            //для разрабов
+            //автоматическое вбивание
+            /*
+            textBox1.Text = "12";
+            textBox2.Text = "12";
+            textBox3.Text = "120";
+            */
+        }
+        //Автор Дамир Алмаев Маратович
+        private void button1_Click(object sender, EventArgs e)//При нажатие кнопки рисуем
+        {
+            try//Попытка
+            {
+                x = Convert.ToInt32(textBox1.Text);//Водим инфу
+                y = Convert.ToInt32(textBox2.Text);//Водим инфу
+                a = Convert.ToInt32(textBox3.Text);//Водим инфу
+                pictureBox1.Refresh();//Перисование
+            }
+            catch//Ошибка
+            {
+                DialogResult lol = MessageBox.Show("Error: Ошибка инфы", this.Text, MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);//Выводим сообщение о ошибки
+                if (lol == DialogResult.Retry)//Если нажата кнопка Повторить, то
+                {
+                    try//Попытка
+                    {
+                        x = Convert.ToInt32(textBox1.Text);//Водим инфу
+                        y = Convert.ToInt32(textBox2.Text);//Водим инфу
+                        a = Convert.ToInt32(textBox3.Text);//Водим инфу
+                    }
+                    catch//Ошибка
+                    {
+                        button1.PerformClick();//Нажимаем на кнопку (автомат)
+                    }
+                }
+            }
+        }
+        //Автор Дамир Алмаев Маратович
+        public Form1()//Загружаем форму
+        {
+            InitializeComponent();//Загружаем форму
+        }
+    }
+}
+//Автор Дамир Алмаев Маратович
+//Конец
